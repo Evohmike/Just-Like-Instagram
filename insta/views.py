3d73ade0eb177a3846434b8d2a1e480486c33b9d
Evohmike <evohmik@gmail.com>
2018-10-04 10:36:23 +0300

    made views to display home page

--- insta/views.py
+++ insta/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+def index(request):
+    return render(request, 'index.html')
