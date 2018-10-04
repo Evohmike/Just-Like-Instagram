4b89f52b745d9c6d0df915b75e3c0ef0dcbfe1f6
Evohmike <evohmik@gmail.com>
2018-10-04 15:07:57 +0300

    modified views

--- insta/views.py
+++ insta/views.py
@@ -27,7 +27,7 @@ def signup(request):
             user.save()
             current_site = get_current_site(request)
             mail_subject = 'Activate your account.'
-            message = render_to_string('.html',{
+            message = render_to_string('acc_activate_email.html',{
                 'user':user,
                 'domain': current_site.domain,
                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
