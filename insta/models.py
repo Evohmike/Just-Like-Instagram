0e55f85d469cba82572051eb2400e342fdde6bf0
Evohmike <evohmik@gmail.com>
2018-10-04 10:35:05 +0300

    created Profile and post class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+  Name = models.TextField(default="Anonymous")
+  profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
+  bio = models.TextField(default="Welcome Me!")
+
+
+
+class Post(models.Model):
+  image = models.ImageField(upload_to='posts/')
+  user = models.ForeignKey(Profile, related_name='posts')
+
+
+
