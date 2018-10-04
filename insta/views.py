8eef05f9f19bbc019c335aa506b5a7a3d835479d
Evohmike <evohmik@gmail.com>
2018-10-04 14:06:41 +0300

    imported classes from models

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    images=Post.objects.all()
+    return render(request, 'index.html',{'images':images,})
