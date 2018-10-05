8f74de419ff1ad0529535d1b52b5c2c463606e21
Evohmike <evohmik@gmail.com>
2018-10-05 23:25:00 +0300

    created profile url

--- insta/urls.py
+++ insta/urls.py
@@ -4,10 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
     url(r'^$', views.signup, name='signup'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    url(r'^profile/$',views.profile,name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
