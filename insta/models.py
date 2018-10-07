27aa3d5c0853c18dc216ee250e7432a08c22d106
Evohmike <evohmik@gmail.com>
2018-10-07 19:40:08 +0300

    modified models

--- insta/models.py
+++ insta/models.py
@@ -5,9 +5,27 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-  Name = models.TextField(default="Anonymous")
+  Name = models.TextField(default="Mike")
   profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
-  bio = models.TextField(default="Welcome Me!")
+  bio = models.TextField(default="I love instagram!")
+
+  def save_profile(self):
+      self.save()
+  
+  @classmethod
+  def search_profile(cls, name):
+      profile = Profile.objects.filter(user__username__icontains = name)
+      return profile
+  
+  @classmethod
+  def get_by_id(cls, id):
+      profile = Profile.objects.get(user = id)
+      return profile
+
+  @classmethod
+  def filter_by_id(cls, id):
+      profile = Profile.objects.filter(user = id).first()
+      return profile
 
 
 
