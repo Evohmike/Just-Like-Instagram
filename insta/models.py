20fa898a9dbb66c055c5f6c7205de32f1b353433
Evohmike <evohmik@gmail.com>
2018-10-08 23:12:45 +0300

    created comment class

--- insta/models.py
+++ insta/models.py
@@ -34,4 +34,14 @@ class Post(models.Model):
   user = models.ForeignKey(Profile, related_name='posts')
 
 
+class Comment(models.Model):
+    user= models.ForeignKey(User)
+    post=models.ForeignKey('Post',null=True)
+    comment= models.CharField(max_length=100)
+    posted_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
+
+
 
