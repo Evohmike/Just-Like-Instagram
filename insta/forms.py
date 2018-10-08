71d07540d4a65facd3b4c1ab9ce10880330c74e2
Evohmike <evohmik@gmail.com>
2018-10-08 23:12:09 +0300

    ted comments form

--- insta/forms.py
+++ insta/forms.py
@@ -1,9 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import *
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user']
