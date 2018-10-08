4344fedb45318d76aa4efbce8cb090643c4a67bb
Evohmike <evohmik@gmail.com>
2018-10-08 23:13:21 +0300

    added post image url

--- insta/urls.py
+++ insta/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^profile/$',views.profile,name='profile'),
+    url(r'^comment/(?P<post_id>\d+)', views.add_comment, name='comment'),
+    url(r'^new/image$', views.new_image, name='new-image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
