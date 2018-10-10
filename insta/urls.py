4e254579a20ff91916772b45a54ce90cb274ff9e
Evohmike <evohmik@gmail.com>
2018-10-10 10:57:49 +0300

    created login url

--- insta/urls.py
+++ insta/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.signup, name='signup'),
-    url(r'^index/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    url(r'^signup$', views.signup, name='signup'),
     url(r'^profile/$',views.profile,name='profile'),
     url(r'^comment/(?P<post_id>\d+)', views.add_comment, name='comment'),
     url(r'^new/image$', views.new_image, name='new-image'),
