d5b05ca67e82b04ec9fd762468d59b2299049d2d
Evohmike <evohmik@gmail.com>
2018-10-12 11:54:24 +0300

    modified models

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -27,6 +41,10 @@ class Profile(models.Model):
       profile = Profile.objects.filter(user = id).first()
       return profile
 
+  @classmethod
+  def find_profile(cls,name):
+      return cls.objects.filter(user__username__icontains = name).all()
+
 
 
 class Post(models.Model):
@@ -47,5 +65,6 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment
 
-
+    # class Meta:
+    #     ordering = ['-']
 
